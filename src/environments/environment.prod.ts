ec5d0d7f46a348603dabedc845537e7d5030bcf1
ChamodKanishka <chamodkanishka77@gmail.com>
2019-03-14 16:11:04 +0530

    initialize structure

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyAq4FIVJZ90g3SYuzI-FVVrTN6oGIl1W_w",
+    authDomain: "food-shop-de8be.firebaseapp.com",
+    databaseURL: "https://food-shop-de8be.firebaseio.com",
+    projectId: "food-shop-de8be",
+    storageBucket: "food-shop-de8be.appspot.com",
+    messagingSenderId: "489016143524"
+
+  }
+};
